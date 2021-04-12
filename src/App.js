471a38922b32da493ea8eab5da4852398c6b7aec
Kapirka <68941822+KapirkaZ@users.noreply.github.com>
2021-04-12 12:18:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import searchImgApi from "./services/searchImg-api";
+import Searchbar from "./components/Searchbar";
+import ImageGallery from "./components/ImageGallery";
+import Button from "./components/Button";
+import Loader from "./components/Loader";
+import Modal from "./components/Modal";
+
+function App() {
+  return <></>;
+}
+
+export default App;
