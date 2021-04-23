19f344022793bc9c7eaa6a0500cf82ecfed7a268
Kapirka <68941822+KapirkaZ@users.noreply.github.com>
2021-04-23 20:55:10 +0300

    Api-key udated

--- src/services/searchImg-api.js
+++ src/services/searchImg-api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API_KEY = "81ed01ec72254f788a832404a1da2109";
-const API_KEY = "20192065-81ed01ec72254f788a832404a1da2109";
+const API_KEY = "21303278-c935acbd5da792aa991cc27e4";
 export const getImages = (query = "", page = 1) => {
   const path = `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
   return axios.get(path);
